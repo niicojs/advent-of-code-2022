04038d5952a5d587bcc79194ffa256bb8a584797
niicojs <5653050+niicojs@users.noreply.github.com>
2022-12-11 20:22:53 +0100

    10 part 2

--- 10.js
+++ 10.js
@@ -5,9 +5,14 @@ const instructions = (await fs.readFile('inputs/10', 'utf8')).split(/\r?\n/g);
 let x = 1;
 let cycle = 0;
 let sum = 0;
+const screen = Array(40 * 6).fill('.');
 
 const tick = () => {
   cycle++;
+  const pos = cycle % 40;
+  if (pos >= x && pos <= x + 2) {
+    screen[cycle] = '#';
+  }
   if ((cycle - 20) % 40 === 0) {
     const strength = cycle * x;
     console.log('cycle ', cycle, x, strength);
@@ -31,3 +36,12 @@ for (const inst of instructions) {
 console.log('x', x);
 console.log('cycle', cycle);
 console.log('(answer 1)', sum);
+
+console.log('(answer 2)');
+for (let y = 0; y < 6; y++) {
+  let line = '';
+  for (let x = 0; x < 40; x++) {
+    line += screen[y * 40 + x];
+  }
+  console.log(line);
+}
