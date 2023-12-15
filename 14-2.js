d288c071838056681d71e255e33c205a1eb7f8d5
niico <5653050+niicojs@users.noreply.github.com>
2023-12-15 22:04:16 +0100

    14

---
+++ 14-2.js
@@ -0,0 +1,121 @@
+import { config } from 'dotenv';
+import { consola } from 'consola';
+import { getCurrentDay, getDataLines, inGridRange } from './src/utils.js';
+import { submit } from './src/aoc.js';
+
+config();
+const day = getCurrentDay();
+
+consola.wrapAll();
+consola.start('Starting day ' + day);
+
+const lines = getDataLines(day).map((l) => l.split(' -> '));
+const traces = [];
+
+// parse data
+for (const line of lines) {
+  for (let i = 0; i < line.length - 1; i++) {
+    const from = line[i].split(',').map((n) => +n);
+    const to = line[i + 1].split(',').map((n) => +n);
+    if (from[0] < to[0] || from[1] < to[1]) {
+      traces.push({ from, to });
+    } else {
+      traces.push({ from: to, to: from });
+    }
+  }
+}
+
+// normalize on 0 based grid
+let maxx = 0;
+let maxy = 0;
+
+for (const trace of traces) {
+  maxx = Math.max(maxx, trace.from[0], trace.to[0]);
+  maxy = Math.max(maxy, trace.from[1], trace.to[1]);
+}
+
+let minx = Math.min(...traces.flatMap((t) => [t.from[0], t.to[0]])) - maxy - 5;
+
+maxx -= minx;
+for (const trace of traces) {
+  trace.from[0] -= minx;
+  trace.to[0] -= minx;
+}
+
+maxx += maxy + 5;
+maxy += 3;
+
+const grid = Array(maxy)
+  .fill(0)
+  .map(() => Array(maxx).fill('.'));
+
+// put stone on grid
+for (const trace of traces) {
+  for (let x = trace.from[0]; x <= trace.to[0]; x++) {
+    for (let y = trace.from[1]; y <= trace.to[1]; y++) {
+      grid[y][x] = '#';
+    }
+  }
+}
+for (let x = 0; x < grid[0].length; x++) {
+  grid[grid.length - 1][x] = '#';
+}
+
+const print = () => {
+  const pad = (grid.length - 1).toString().length;
+  for (let y = 0; y < grid.length; y++) {
+    let line = y.toString().padStart(pad, ' ') + ' ';
+    for (let x = 0; x < grid[y].length; x++) {
+      if (y === 0 && x === 500 - minx) {
+        line += '+';
+      } else {
+        line += grid[y][x];
+      }
+    }
+    consola.log(line);
+  }
+};
+
+print();
+
+const step = () => {
+  const pos = { x: 500 - minx, y: 0 };
+  while (true) {
+    while (
+      inGridRange(grid, pos.x, pos.y + 1) &&
+      grid[pos.y + 1][pos.x] === '.'
+    ) {
+      pos.y++;
+    }
+    if (
+      inGridRange(grid, pos.x - 1, pos.y + 1) &&
+      grid[pos.y + 1][pos.x - 1] === '.'
+    ) {
+      pos.x--;
+      pos.y++;
+    } else if (
+      inGridRange(grid, pos.x + 1, pos.y + 1) &&
+      grid[pos.y + 1][pos.x + 1] === '.'
+    ) {
+      pos.x++;
+      pos.y++;
+    } else {
+      break;
+    }
+  }
+  grid[pos.y][pos.x] = 'o';
+  return pos.y !== 0;
+};
+
+let cnt = 1;
+while (step()) {
+  cnt++;
+}
+
+print();
+
+consola.warn('part 2', cnt);
+
+// await submit({ day, level: 2, answer: cnt });
+
+consola.success('Done.');
